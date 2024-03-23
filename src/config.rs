da9a0516dca1e197718b7c4b01cce60a8647467f
Axel Örn Sigurðsson <git@absalon.dev>
2024-03-23 23:21:31 +0000

    Replace anyhow with just basic error handling

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,8 @@
-use anyhow::{Context, Result};
 use base64::prelude::*;
 use std::env;
 use std::sync::OnceLock;
+
+use crate::{NotifluxError, NotifluxErrorType};
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -18,13 +19,18 @@ const DEFAULT_WORKER_COUNT: usize = 4;
 static CONFIG: OnceLock<Config> = OnceLock::new();
 
 impl Config {
-    pub fn init_from_env() -> Result<Self> {
-        let jwt_public_key_b64 =
-            env::var("JWT_PUBLIC_KEY_B64").context("Unable to read JWT_PUBLIC_KEY_B64 from env")?;
+    pub fn init_from_env() -> Result<Self, NotifluxError> {
+        let jwt_public_key_b64 = env::var("JWT_PUBLIC_KEY_B64").map_err(|_| NotifluxError {
+            message: Some("JWT_PUBLIC_KEY_B64 is not set in env".to_string()),
+            error_type: NotifluxErrorType::EnvError,
+        })?;
+
         let jwt_public_key = BASE64_STANDARD
             .decode(jwt_public_key_b64.as_bytes())
-            .context("Unable to Base64 decode JWT_PUBLIC_KEY_B64")?;
-
+            .map_err(|_| NotifluxError {
+                message: Some("Unable to Base64 decode JWT_PUBLIC_KEY_B64".to_string()),
+                error_type: NotifluxErrorType::Base64DecodeError,
+            })?;
         let port = env::var("PORT")
             .unwrap_or_else(|_| DEFAULT_PORT.to_string())
             .parse::<u16>()?;
@@ -43,5 +49,11 @@ impl Config {
 }
 
 pub fn get_config() -> &'static Config {
-    CONFIG.get_or_init(|| Config::init_from_env().expect("Failed to initialize config"))
+    CONFIG.get_or_init(|| match Config::init_from_env() {
+        Ok(config) => config,
+        Err(e) => {
+            log::error!("Error initializing config: {}", e);
+            std::process::exit(1);
+        }
+    })
 }
